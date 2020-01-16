fd1be0d8281c3c539cc5cd90e54ca4d498eec795
Janis Dimants <dimants.janis99@gmail.com>
2020-01-16 10:54:51 +0200

    Remove unneccesary import

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-import { useRef } from "react";
 
 const config = {
   apiKey: "AIzaSyDTUtw5seDRJPD_GQh0S5-FJ-QemDPce04",
